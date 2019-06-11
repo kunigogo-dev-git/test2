3d945f7337bc37408e5980dd6860df5bdfba9d47
kunigogo-dev-git <kunigogo.dev.git@gmail.com>
2019-06-11 02:08:41 +0000

    wip added data-table

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,25 +4,38 @@
       text-xs-center
       wrap
     >
-      <v-flex xs12>
+      <v-flex xs8>
+        <v-card width="100%" height="100%" min-height="300px">
+        <v-system-bar lighter>
+          <span>Data List</span>
+        </v-system-bar>
+        <v-data-table
+          :headers="headers"
+          :items="dataList"
+          class="elevation-1"
+        >
+          <template v-slot:items="props">
+            <td class="text-xs-left">{{ props.item.id }}</td>
+            <td class="text-xs-left">{{ props.item.name }}</td>
+            <td class="text-xs-center">{{ props.item.age }}</td>
+            <td class="text-xs-left">{{ props.item.studyDate }}</td>
+          </template>
+        </v-data-table>
+        </v-card>
       </v-flex>
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
+      <v-flex xs4>
+        <v-card width="100%" height="100%" min-height="300px">
+          <v-system-bar lighter>
+            <span>Import and Information Area</span>
+          </v-system-bar>
+          <v-card-title>Drop to import area</v-card-title>
+          <v-divider></v-divider>
+        <v-card-text></v-card-text>
+        </v-card>
       </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
-      >
-      </v-flex>
+
     </v-layout>
   </v-container>
 </template>
@@ -30,57 +43,17 @@
 <script>
   export default {
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
+      headers: [
+        { text: 'ID', align: 'center', value: 'id' },
+        { text: 'Name', align: 'center', value: 'name' },
+        { text: 'Age', align: 'center', value: 'age' },
+        { text: 'Study Date', align: 'center', value: 'studyDate' },
+        { text: 'Operation', align: 'center', value: 'action', sortable: false }
       ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
+      dataList: [
+        { id: 'P00001', name: 'Pat 1', age: 10, studyDate: '2019/04/02' },
+        { id: 'P03043', name: 'Pat 2', age: 10, studyDate: '2004/08/02' },
       ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
-
-      ]
     })
   }
 </script>
